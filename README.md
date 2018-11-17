4ce0621060882857bc34a1218f1e4d9003fb837b
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2018-11-17 20:17:10 +0400

    Add zola package

--- README.md
+++ README.md
@@ -14,13 +14,13 @@ Pack
 To pack the package, issue the following command like this:
 
 ```console
-> choco pack ./gutenberg/gutenberg.nuspec
+> choco pack ./zola/zola.nuspec
 ```
 
 To install the resulting package (for example, to test it):
 
 ```console
-> choco install ./gutenberg.0.4.2.nupkg -f
+> choco install ./zola.0.5.0.nupkg -f
 ```
 
 Push
@@ -29,5 +29,5 @@ Push
 To push the package to Chocolatey.org, invoke the following:
 
 ```console
-> choco push ./gutenberg.0.4.2.nupkg --api-key [your API key]
+> choco push ./zola.0.5.0.nupkg --api-key [your API key]
 ```
---
+++ zola/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'
+
+$packageName= $env:ChocolateyPackageName
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url      = "https://github.com/getzola/zola/releases/download/v$($env:ChocolateyPackageVersion)/zola-v$($env:ChocolateyPackageVersion)-x86_64-pc-windows-msvc.zip"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  checksum      = '492CCEA9C52CF8026352488B6064B02CA91AE93A9E70F34D09928868B2F0F011'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
